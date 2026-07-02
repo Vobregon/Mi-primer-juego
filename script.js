35b3c2e0dc9aba351dd0e61c2afdc15bc68d8b29
Vobregon <vihobregon@gmail.com>
2026-07-01 23:15:23 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,120 @@
+
+
+
+
+let secreto=Math.floor(Math.random()*100);
+
+console.log(secreto);
+alert("El numero secreto es: " + secreto);
+
+//const secreto = 37;
+
+let intento1 = Number(prompt("Intento 1: Ingresa numero secreto:"));
+
+if(intento1 === secreto){
+    console.log("Felicidades, el numero es correcto")
+    alert("Felicidades, el numero es correcto !!!")
+} else if (intento1 > secreto) {
+    console.log("El numero, es muy alto")
+    alert("El numero es muy alto !!!")
+} else {
+    console.log("El numero, es muy bajo")
+    alert("El numero es muy bajo")
+}
+
+let intento2 = Number(prompt("Intento 2: Ingresa numero secreto:"));
+
+if(intento2 === secreto){
+    console.log("Felicidades, el numero es correcto")
+    alert("Felicidades, el numero es correcto !!!")
+} else if (intento2 > secreto) {
+    console.log("El numero, es muy alto")
+    alert("El numero es muy alto !!!")
+} else {
+    console.log("El numero, es muy bajo")
+    alert("El numero es muy bajo")
+}
+let intento3 = Number(prompt("Intento 3: Ingresa numero secreto:"));
+
+if(intento3 === secreto){
+    console.log("Felicidades, el numero es correcto")
+    alert("Felicidades, el numero es correcto !!!")
+} else if (intento3 > secreto) {
+    console.log("El numero, es muy alto")
+    alert("El numero es muy alto !!!")
+} else {
+    console.log("El numero, es muy bajo")
+    alert("El numero es muy bajo")
+}
+
+
+//let secreto=Math.floor(Math.random()*100);
+
+//console.log(secreto);
+//alert("El numero es: " + numero);
+/*
+const secreto = 60;
+let entrada = prompt("Ingrese un numero:");
+
+let numero = Number(entrada);
+
+if (isNaN(numero)){
+  alert("No es un numero, Recarga para volver a intentarlo");
+} else if(numero < 1 || numero >100){
+   alert("Ese numero esta fuera del rango, recuerda que debe ser entre 1 y 100. Recarga por favor");
+} else if (numero === secreto){
+ alert("Felicidades, el numero es correcto !!!")
+}else if(numero > secreto) {
+   alert("El numero es muy alto !!!")
+} else {
+     alert("El numero es muy bajo")
+}
+*/
+
+
+/*let numero = prompt("Dame un numero");
+console.log(numero)
+console.log(typeof numero)
+console.log(numero * 10)
+console.log(numero + 10)
+
+
+let nombre = 'Victor';
+let edad = 57;
+let programador = true;
+
+console.log(nombre, typeof nombre);
+console.log(edad, typeof edad);
+console.log(programador, typeof programador);
+
+
+let msg1 = "Hola soy " + nombre + ", tengo " + edad + " años y soy programador" ;
+
+console.log(msg1);
+
+
+let msg2 = `Hola, soy  ${nombre} , tengo ${edad}  años y soy programador` ;
+console.log(msg2);
+
+*/
+
+/*
+
+
+// 1. Solicitamos los dos números y los convertimos a números reales
+let numero1 = Number(prompt("Ingresa el primer número:"));
+let numero2 = Number(prompt("Ingresa el segundo número:"));
+
+
+
+// 2. Realizamos la suma
+let resultado = numero1 + numero2;
+
+// 3. Mostramos el resultado
+alert("La suma es: " + resultado);*/
+
+
+//let numero=Math.floor(Math.random()*100);
+
+//console.log(numero);
+//alert("El numero es: " + numero);
